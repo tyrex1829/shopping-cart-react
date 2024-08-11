2d2a0f35c47a6fccb34799381b3d58325636013b
tyrex1829 <sakshamkhare19@gmail.com>
2024-08-11 18:43:57 +0530

    navbar complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,26 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 
-function Navbar() {
+function Navbar({ size, setShow }) {
   return (
     <nav className="rounded-lg flex justify-between items-center bg-blue-600 py-5 px-10 text-white">
-      <div className="font-bold text-3xl cursor-pointer">Shopping cart</div>
-      <div className=" cursor-pointer">
+      <div
+        className="font-bold text-3xl cursor-pointer"
+        onClick={() => {
+          setShow(true);
+        }}
+      >
+        Shopping cart
+      </div>
+      <div
+        className=" cursor-pointer"
+        onClick={() => {
+          setShow(false);
+        }}
+      >
         <FontAwesomeIcon className="text-3xl" icon={faCartShopping} />
         <span className=" bg-red-600 px-2 font-semibold text-white rounded-3xl text-xl relative top-[-15px]">
-          0
+          {size}
         </span>
       </div>
     </nav>
