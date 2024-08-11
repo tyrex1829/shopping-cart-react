9e2ee13bb1ddf0d4d9b73149eb71bf77b0c0e0d8
tyrex1829 <sakshamkhare19@gmail.com>
2024-08-11 18:46:34 +0530

    shopping cart ugly code

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,41 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import ItemGrid from "./components/ItemGrid";
+import Cart from "./components/Cart";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [cart, setCart] = useState([]);
+  const [warning, setWarning] = useState(false);
+  const [show, setShow] = useState(true);
+
+  const handleClick = (item) => {
+    let isPresent = false;
+    cart.forEach((product) => {
+      if (item.id === product.id) {
+        isPresent = true;
+      }
+    });
+    if (isPresent) {
+      setWarning(true);
+      setTimeout(() => {
+        setWarning(false);
+      }, 2000);
+      return;
+    }
+
+    setCart([...cart, item]);
+  };
 
   return (
     <div className="max-w-screen-2xl mx-auto mt-5 bg-white">
-      <Navbar />
-      <ItemGrid />
+      <Navbar size={cart.length} setShow={setShow} />
+      {show ? (
+        <ItemGrid handleClick={handleClick} />
+      ) : (
+        <Cart cart={cart} setCart={setCart} />
+      )}
+
+      {warning && alert("Item is already present in your cart")}
     </div>
   );
 }
