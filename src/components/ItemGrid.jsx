a1470e14f5defd16febc26d3858db184f36818df
tyrex1829 <sakshamkhare19@gmail.com>
2024-08-11 18:44:54 +0530

    prop drilling code of item grid

--- src/components/ItemGrid.jsx
+++ src/components/ItemGrid.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import list from "../listOfItems";
 import ItemCard from "./ItemCard";
 
-function ItemGrid() {
+function ItemGrid({ handleClick }) {
   return (
     <section className="py-11 px-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-8 gap-y-5">
       {list.map((i) => (
         <ItemCard
+          item={i}
+          handleClick={handleClick}
           key={i.id}
           image={i.img}
           title={i.title}
